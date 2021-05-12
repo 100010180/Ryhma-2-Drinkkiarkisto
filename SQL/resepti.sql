3e837e66eb824235c673ea1740243f3f49afe37c
800010180 <83632505+800010180@users.noreply.github.com>
2021-05-12 11:46:28 +0300

    Add files via upload

---
+++ SQL/resepti.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2021 at 10:46 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `drinkkiarkisto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resepti`
+--
+
+CREATE TABLE `resepti` (
+  `Resepti_id` int(11) NOT NULL,
+  `Nimi` varchar(50) NOT NULL,
+  `Juomalaji` varchar(50) NOT NULL,
+  `Ainesosat` varchar(100) NOT NULL,
+  `Valmistusohje` text NOT NULL,
+  `Kayttaja_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
