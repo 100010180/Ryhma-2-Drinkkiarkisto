819db14c4e3a4e7df36f14d83900a77db16fdfa6
800010180 <83632505+800010180@users.noreply.github.com>
2021-05-12 11:40:26 +0300

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,215 +1,91 @@
-<?php
-ini_set('display_errors', 0);
+<html>
+<head>
+</head>
+<body>
+<center>
+<script language="javascript" type="text/javascript">
+var day_of_week = new Array('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
+var month_of_year = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
 
-class Calendar {
-	
-     
-    /**
-     * Constructor
-     */
-    public function __construct(){     
-        $this->naviHref = htmlentities($_SERVER['PHP_SELF']);
-    }
-    
-    /********************* PROPERTY ********************/  
-    private $dayLabels = array("Mon","Tue","Wed","Thu","Fri","Sat","Sun");
-     
-    private $currentYear=0;
-     
-    private $currentMonth=0;
-     
-    private $currentDay=0;
-     
-    private $currentDate=null;
-     
-    private $daysInMonth=0;
-     
-    private $naviHref= null;
-     
-    /********************* PUBLIC **********************/  
-        
-    /**
-    * print out the calendar
-    */
-    public function show() {
-        $year  = null;
-         
-        $month = null;
-         
-        if(null==$year&&isset($_GET['year'])){
- 
-            $year = $_GET['year'];
-         
-        }else if(null==$year){
- 
-            $year = date("Y",time());  
-         
-        }          
-         
-        if(null==$month&&isset($_GET['month'])){
- 
-            $month = $_GET['month'];
-         
-        }else if(null==$month){
- 
-            $month = date("m",time());
-         
-        }                  
-         
-        $this->currentYear=$year;
-         
-        $this->currentMonth=$month;
-         
-        $this->daysInMonth=$this->_daysInMonth($month,$year);  
-         
-        $content='<div id="calendar">'.
-                        '<div class="box">'.
-                        $this->_createNavi().
-                        '</div>'.
-                        '<div class="box-content">'.
-                                '<ul class="label">'.$this->_createLabels().'</ul>';   
-                                $content.='<div class="clear"></div>';     
-                                $content.='<ul class="dates">';    
-                                 
-                                $weeksInMonth = $this->_weeksInMonth($month,$year);
-                                // Create weeks in a month
-                                for( $i=0; $i<$weeksInMonth; $i++ ){
-                                     
-                                    //Create days in a week
-                                    for($j=1;$j<=7;$j++){
-                                        $content.=$this->_showDay($i*7+$j);
-                                    }
-                                }
-                                 
-                                $content.='</ul>';
-                                 
-                                $content.='<div class="clear"></div>';     
-             
-                        $content.='</div>';
-                 
-        $content.='</div>';
-        return $content;   
-    }
-     
-    /********************* PRIVATE **********************/ 
-    /**
-    * create the li element for ul
-    */
-    private function _showDay($cellNumber){
-         
-        if($this->currentDay==0){
-             
-            $firstDayOfTheWeek = date('N',strtotime($this->currentYear.'-'.$this->currentMonth.'-01'));
-                     
-            if(intval($cellNumber) == intval($firstDayOfTheWeek)){
-                 
-                $this->currentDay=1;
-                 
-            }
-        }
-         
-        if( ($this->currentDay!=0)&&($this->currentDay<=$this->daysInMonth) ){
-             
-            $this->currentDate = date('Y-m-d',strtotime($this->currentYear.'-'.$this->currentMonth.'-'.($this->currentDay)));
-             
-            $cellContent = $this->currentDay;
-             
-            $this->currentDay++;   
-             
-        }else{
-             
-            $this->currentDate =null;
- 
-            $cellContent=null;
-        }
-             
-         
-        return '<li id="li-'.$this->currentDate.'" class="'.($cellNumber%7==1?' start ':($cellNumber%7==0?' end ':' ')).
-                ($cellContent==null?'mask':'').'">'.$cellContent.'</li>';
-    }
-     
-    /**
-    * create navigation
-    */
-    private function _createNavi(){
-         
-        $nextMonth = $this->currentMonth==12?1:intval($this->currentMonth)+1;
-         
-        $nextYear = $this->currentMonth==12?intval($this->currentYear)+1:$this->currentYear;
-         
-        $preMonth = $this->currentMonth==1?12:intval($this->currentMonth)-1;
-         
-        $preYear = $this->currentMonth==1?intval($this->currentYear)-1:$this->currentYear;
-         
-        return
-            '<div class="header">'.
-                '<a class="prev" href="'.$this->naviHref.'?month='.sprintf('%02d',$preMonth).'&year='.$preYear.'">Prev</a>'.
-                    '<span class="title">'.date('Y M',strtotime($this->currentYear.'-'.$this->currentMonth.'-1')).'</span>'.
-                '<a class="next" href="'.$this->naviHref.'?month='.sprintf("%02d", $nextMonth).'&year='.$nextYear.'">Next</a>'.
-            '</div>';
-    }
-         
-    /**
-    * create calendar week labels
-    */
-    private function _createLabels(){  
-                 
-        $content='';
-         
-        foreach($this->dayLabels as $indexx=>$label){
-             
-            $content.='<li class="'.($label==6?'end title':'start title').' title">'.$label.'</li>';
- 
-        }
-         
-        return $content;
-    }
-     
-     
-     
-    /**
-    * calculate number of weeks in a particular month
-    */
-    private function _weeksInMonth($month=null,$year=null){
-         
-        if( null==($year) ) {
-            $year =  date("Y",time()); 
-        }
-         
-        if(null==($month)) {
-            $month = date("m",time());
-        }
-         
-        // find number of days in this month
-        $daysInMonths = $this->_daysInMonth($month,$year);
-         
-        $numOfweeks = ($daysInMonths%7==0?0:1) + intval($daysInMonths/7);
-         
-        $monthEndingDay= date('N',strtotime($year.'-'.$month.'-'.$daysInMonths));
-         
-        $monthStartDay = date('N',strtotime($year.'-'.$month.'-01'));
-         
-        if($monthEndingDay<$monthStartDay){
-             
-            $numOfweeks++;
-         
-        }
-         
-        return $numOfweeks;
-    }
- 
-    /**
-    * calculate number of days in a particular month
-    */
-    private function _daysInMonth($month=null,$year=null){
-         
-        if(null==($year))
-            $year =  date("Y",time()); 
- 
-        if(null==($month))
-            $month = date("m",time());
-             
-        return date('t',strtotime($year.'-'.$month.'-01'));
-    }
-     
+var Calendar = new Date();
+
+var year = Calendar.getFullYear();     // Returns year
+var month = Calendar.getMonth();    // Returns month (0-11)
+var today = Calendar.getDate();    // Returns day (1-31)
+var weekday = Calendar.getDay();    // Returns day (1-31)
+
+var DAYS_OF_WEEK = 7;
+var DAYS_OF_MONTH = 31;
+var cal;
+
+Calendar.setDate(1);
+Calendar.setMonth(month);
+
+var TR_start = '<TR>';
+var TR_end = '</TR>';
+var highlight_start = '<TD WIDTH="30"><TABLE CELLSPACING=0 BORDER=1 BGCOLOR=gray BORDERCOLOR=white><TR><TD WIDTH=35><B><CENTER>';
+var highlight_end   = '</CENTER></TD></TR></TABLE></B>';
+var TD_start = '<TD WIDTH="50"><CENTER>';
+var TD_end = '</CENTER></TD>';
+
+cal =  '<TABLE BORDER=10 CELLSPACING=0 BGCOLOR=gray CELLPADDING=0 BORDERCOLOR=white><TR><TD>';
+cal += '<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=15 BORDERCOLOR=white>' + TR_start;
+cal += '<TD COLSPAN="' + DAYS_OF_WEEK + '" BGCOLOR="white"><CENTER><B>';
+cal += month_of_year[month]  + '   ' + year + '</B>' + TD_end + TR_end;
+cal += TR_start;
+
+for(index=0; index < DAYS_OF_WEEK; index++)
+{
+
+if(weekday == index)
+cal += TD_start + '<B>' + day_of_week[index] + '</B>' + TD_end;
+
+else
+cal += TD_start + day_of_week[index] + TD_end;
 }
+
+cal += TD_end + TR_end;
+cal += TR_start;
+
+for(index=0; index < Calendar.getDay(); index++)
+cal += TD_start + '  ' + TD_end;
+
+for(index=0; index < DAYS_OF_MONTH; index++)
+{
+if( Calendar.getDate() > index )
+{
+	
+  week_day =Calendar.getDay();
+  
+  if(week_day == 0)
+  cal += TR_start;
+
+  if(week_day != DAYS_OF_WEEK)
+  {
+
+  var day  = Calendar.getDate();
+
+  if( today==Calendar.getDate() )
+  cal += highlight_start + day + highlight_end + TD_end;
+
+  else
+  cal += TD_start + day + TD_end;
+  }
+
+  if(week_day == DAYS_OF_WEEK)
+  cal += TR_end;
+  }
+
+  Calendar.setDate(Calendar.getDate()+1);
+
+}
+
+cal += '</TD></TR></TABLE></TABLE>';
+
+document.write(cal);
+
+</script>
+</center>
+<br/><div style="clear:both"></div>
+</body>
+</html>
